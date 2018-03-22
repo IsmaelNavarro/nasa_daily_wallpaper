55d7525a5d854b6c5d740d0e954b773c555d0456
Ismael Navarro <ismanapa@gmail.com>
2018-03-23 00:22:24 +0100

    Wallpaper changer

---
+++ config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    API_KEY: "IG0DL29wgXTWgfuWE2KwKBe8emBfa4F3swBaJ32U",
+    API_ENDPOINT: "https://api.nasa.gov/planetary/apod?api_key="
+};
---
+++ index.js
@@ -0,0 +1,36 @@
+const wallpaper = require('wallpaper');
+const axios = require('axios');
+const fs = require('fs');
+const sanitize = require("sanitize-filename");
+const config = require('./config');
+
+axios.get(`${config.API_ENDPOINT}${config.API_KEY}`)
+    .then(response => {
+        const { data } = response;
+        const title = sanitize(data.title);
+        const file = `./wallpapers/${title}.jpg`;
+
+        axios({
+            method: 'get',
+            url: data.hdurl,
+            responseType: 'stream'
+        })
+            .then(response => {
+                let stream = fs.createWriteStream(file);
+                response
+                    .data
+                    .pipe(stream);
+
+                stream.on('finish', function () {
+                    wallpaper.set(file).then(() => {
+                        console.log('wallpaper changed!');
+                    });
+                });
+            });
+
+    })
+    .catch(function (error) {
+        console.error(error);
+    });
+
+
